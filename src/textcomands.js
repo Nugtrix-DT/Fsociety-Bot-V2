e815ef2afe98cd65053530a1ae88e7ed9bcd2920
Nugtrix-DT <80982110+Nugtrix-DT@users.noreply.github.com>
2021-04-16 12:56:34 -0500

    Add files via upload

---
+++ src/textcomands.js
@@ -0,0 +1,15 @@
+const textcomands = (p) => {
+return `
+༎░ ▁▂▃༻ʄʂơƈıɛɬყ ცơɬ ۷2༺▃▂▁ ░༎
+╔════ ❯ Texto ❮ ══════════
+║ ❯ *${p}glitch* <text1> | <text2>
+║ ❯ *${p}ravetext* <text1> | <text2>
+║ ❯ *${p}woodtext* <text1> | <text2>
+║ ❯ *${p}neon* <text1> | <text2>
+║ ❯ *${p}sunset* <text1> | <text2>
+║ ❯ *${p}gimage* <text>
+║ ❯ *${p}whatis* <text>
+║ ❯ *${p}text3d* <text>
+╚═══════════════════════════`
+}
+exports.textcomands = textcomands
